8fe43e19a85b606e5c8a18d061c4ecbc648ad773
ejtho <ejthompson03@gmail.com>
2017-02-21 17:13:16 -0500

    changes n shit

--- app/src/main/java/com/example/ejtho/androidprojecttwo/MainActivity.java
+++ app/src/main/java/com/example/ejtho/androidprojecttwo/MainActivity.java
@@ -3,15 +3,13 @@ package com.example.ejtho.androidprojecttwo;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 
 public class MainActivity extends AppCompatActivity {
 
     private int screenheight;
     private int screenwidth;
-
-    private SharedPreferences.OnSharedPreferenceChangeListener myListener;
-    private SharedPreferences myPreferences;
 
     //TODO Figure out all this permissions nonsense
 
